3df2a89dfb0510ab74dbb2bdd470e64a8cbce118
Damien Laitani <d.laitani@gmail.com>
2023-01-09 18:49:44 +0100

    calculatrice v1

---
+++ calculatrice/Form1.Designer.vb
@@ -0,0 +1,389 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Result = New System.Windows.Forms.Button()
+        Me.Button04 = New System.Windows.Forms.Button()
+        Me.Button02 = New System.Windows.Forms.Button()
+        Me.Button03 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button05 = New System.Windows.Forms.Button()
+        Me.Button06 = New System.Windows.Forms.Button()
+        Me.Button01 = New System.Windows.Forms.Button()
+        Me.Button07 = New System.Windows.Forms.Button()
+        Me.Button08 = New System.Windows.Forms.Button()
+        Me.Button09 = New System.Windows.Forms.Button()
+        Me.DivButton = New System.Windows.Forms.Button()
+        Me.SubButton = New System.Windows.Forms.Button()
+        Me.PlusButton = New System.Windows.Forms.Button()
+        Me.virgule = New System.Windows.Forms.Button()
+        Me.ResetShow = New System.Windows.Forms.Button()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.RemoveOne = New System.Windows.Forms.Button()
+        Me.showRes = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.MultiButton = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.rightNum = New System.Windows.Forms.Label()
+        Me.leftNum = New System.Windows.Forms.Label()
+        Me.sign = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Result
+        '
+        Me.Result.Location = New System.Drawing.Point(51, 354)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(106, 50)
+        Me.Result.TabIndex = 0
+        Me.Result.Text = "Résultat"
+        Me.Result.UseVisualStyleBackColor = True
+        '
+        'Button04
+        '
+        Me.Button04.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button04.Location = New System.Drawing.Point(51, 242)
+        Me.Button04.Name = "Button04"
+        Me.Button04.Size = New System.Drawing.Size(50, 50)
+        Me.Button04.TabIndex = 0
+        Me.Button04.Text = "4"
+        Me.Button04.UseVisualStyleBackColor = True
+        '
+        'Button02
+        '
+        Me.Button02.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button02.Location = New System.Drawing.Point(107, 298)
+        Me.Button02.Name = "Button02"
+        Me.Button02.Size = New System.Drawing.Size(50, 50)
+        Me.Button02.TabIndex = 0
+        Me.Button02.Text = "2"
+        Me.Button02.UseVisualStyleBackColor = True
+        '
+        'Button03
+        '
+        Me.Button03.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button03.Location = New System.Drawing.Point(163, 298)
+        Me.Button03.Name = "Button03"
+        Me.Button03.Size = New System.Drawing.Size(50, 50)
+        Me.Button03.TabIndex = 0
+        Me.Button03.Text = "3"
+        Me.Button03.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(163, 242)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "1"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button05
+        '
+        Me.Button05.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button05.Location = New System.Drawing.Point(107, 242)
+        Me.Button05.Name = "Button05"
+        Me.Button05.Size = New System.Drawing.Size(50, 50)
+        Me.Button05.TabIndex = 0
+        Me.Button05.Text = "5"
+        Me.Button05.UseVisualStyleBackColor = True
+        '
+        'Button06
+        '
+        Me.Button06.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button06.Location = New System.Drawing.Point(163, 242)
+        Me.Button06.Name = "Button06"
+        Me.Button06.Size = New System.Drawing.Size(50, 50)
+        Me.Button06.TabIndex = 0
+        Me.Button06.Text = "6"
+        Me.Button06.UseVisualStyleBackColor = True
+        '
+        'Button01
+        '
+        Me.Button01.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button01.Location = New System.Drawing.Point(51, 298)
+        Me.Button01.Name = "Button01"
+        Me.Button01.Size = New System.Drawing.Size(50, 50)
+        Me.Button01.TabIndex = 0
+        Me.Button01.Text = "1"
+        Me.Button01.UseVisualStyleBackColor = True
+        '
+        'Button07
+        '
+        Me.Button07.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button07.Location = New System.Drawing.Point(51, 186)
+        Me.Button07.Name = "Button07"
+        Me.Button07.Size = New System.Drawing.Size(50, 50)
+        Me.Button07.TabIndex = 0
+        Me.Button07.Text = "7"
+        Me.Button07.UseVisualStyleBackColor = True
+        '
+        'Button08
+        '
+        Me.Button08.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button08.Location = New System.Drawing.Point(107, 186)
+        Me.Button08.Name = "Button08"
+        Me.Button08.Size = New System.Drawing.Size(50, 50)
+        Me.Button08.TabIndex = 0
+        Me.Button08.Text = "8"
+        Me.Button08.UseVisualStyleBackColor = True
+        '
+        'Button09
+        '
+        Me.Button09.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button09.Location = New System.Drawing.Point(163, 186)
+        Me.Button09.Name = "Button09"
+        Me.Button09.Size = New System.Drawing.Size(50, 50)
+        Me.Button09.TabIndex = 0
+        Me.Button09.Text = "9"
+        Me.Button09.UseVisualStyleBackColor = True
+        '
+        'DivButton
+        '
+        Me.DivButton.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DivButton.Location = New System.Drawing.Point(219, 186)
+        Me.DivButton.Name = "DivButton"
+        Me.DivButton.Size = New System.Drawing.Size(50, 50)
+        Me.DivButton.TabIndex = 0
+        Me.DivButton.Text = "÷"
+        Me.DivButton.UseVisualStyleBackColor = True
+        '
+        'SubButton
+        '
+        Me.SubButton.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SubButton.Location = New System.Drawing.Point(219, 242)
+        Me.SubButton.Name = "SubButton"
+        Me.SubButton.Size = New System.Drawing.Size(50, 50)
+        Me.SubButton.TabIndex = 0
+        Me.SubButton.Text = "-"
+        Me.SubButton.UseVisualStyleBackColor = True
+        '
+        'PlusButton
+        '
+        Me.PlusButton.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PlusButton.Location = New System.Drawing.Point(219, 298)
+        Me.PlusButton.Name = "PlusButton"
+        Me.PlusButton.Size = New System.Drawing.Size(50, 50)
+        Me.PlusButton.TabIndex = 0
+        Me.PlusButton.Text = "+"
+        Me.PlusButton.UseVisualStyleBackColor = True
+        '
+        'virgule
+        '
+        Me.virgule.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.virgule.Location = New System.Drawing.Point(219, 354)
+        Me.virgule.Name = "virgule"
+        Me.virgule.Size = New System.Drawing.Size(50, 50)
+        Me.virgule.TabIndex = 0
+        Me.virgule.Text = ","
+        Me.virgule.UseVisualStyleBackColor = True
+        '
+        'ResetShow
+        '
+        Me.ResetShow.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResetShow.Location = New System.Drawing.Point(51, 130)
+        Me.ResetShow.Name = "ResetShow"
+        Me.ResetShow.Size = New System.Drawing.Size(50, 50)
+        Me.ResetShow.TabIndex = 0
+        Me.ResetShow.Text = "CE"
+        Me.ResetShow.UseVisualStyleBackColor = True
+        '
+        'Reset
+        '
+        Me.Reset.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Reset.Location = New System.Drawing.Point(107, 130)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(50, 50)
+        Me.Reset.TabIndex = 0
+        Me.Reset.Text = "C"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'RemoveOne
+        '
+        Me.RemoveOne.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RemoveOne.Location = New System.Drawing.Point(163, 130)
+        Me.RemoveOne.Name = "RemoveOne"
+        Me.RemoveOne.Size = New System.Drawing.Size(50, 50)
+        Me.RemoveOne.TabIndex = 0
+        Me.RemoveOne.Text = "<="
+        Me.RemoveOne.UseVisualStyleBackColor = True
+        '
+        'showRes
+        '
+        Me.showRes.BackColor = System.Drawing.SystemColors.Window
+        Me.showRes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.showRes.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.showRes.Location = New System.Drawing.Point(51, 95)
+        Me.showRes.Name = "showRes"
+        Me.showRes.Size = New System.Drawing.Size(218, 32)
+        Me.showRes.TabIndex = 1
+        Me.showRes.Text = "0"
+        Me.showRes.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(88, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(125, 25)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Calculatrice"
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button0.Location = New System.Drawing.Point(163, 354)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(50, 50)
+        Me.Button0.TabIndex = 0
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'MultiButton
+        '
+        Me.MultiButton.Font = New System.Drawing.Font("Calisto MT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiButton.Location = New System.Drawing.Point(219, 130)
+        Me.MultiButton.Name = "MultiButton"
+        Me.MultiButton.Size = New System.Drawing.Size(50, 50)
+        Me.MultiButton.TabIndex = 0
+        Me.MultiButton.Text = "x"
+        Me.MultiButton.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.AutoSize = True
+        Me.TableLayoutPanel1.ColumnCount = 4
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.Controls.Add(Me.rightNum, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.leftNum, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.sign, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(74, 59)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(195, 33)
+        Me.TableLayoutPanel1.TabIndex = 3
+        '
+        'rightNum
+        '
+        Me.rightNum.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.rightNum.Location = New System.Drawing.Point(74, 0)
+        Me.rightNum.Margin = New System.Windows.Forms.Padding(0)
+        Me.rightNum.Name = "rightNum"
+        Me.rightNum.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.rightNum.Size = New System.Drawing.Size(38, 33)
+        Me.rightNum.TabIndex = 6
+        Me.rightNum.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'leftNum
+        '
+        Me.leftNum.AutoSize = True
+        Me.leftNum.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.leftNum.Location = New System.Drawing.Point(3, 0)
+        Me.leftNum.Name = "leftNum"
+        Me.leftNum.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.leftNum.Size = New System.Drawing.Size(59, 33)
+        Me.leftNum.TabIndex = 4
+        Me.leftNum.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'sign
+        '
+        Me.sign.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.sign.Location = New System.Drawing.Point(37, 0)
+        Me.sign.Margin = New System.Windows.Forms.Padding(0)
+        Me.sign.Name = "sign"
+        Me.sign.Size = New System.Drawing.Size(38, 33)
+        Me.sign.TabIndex = 5
+        Me.sign.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(322, 428)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.showRes)
+        Me.Controls.Add(Me.MultiButton)
+        Me.Controls.Add(Me.PlusButton)
+        Me.Controls.Add(Me.Button09)
+        Me.Controls.Add(Me.SubButton)
+        Me.Controls.Add(Me.Button06)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button08)
+        Me.Controls.Add(Me.Button05)
+        Me.Controls.Add(Me.RemoveOne)
+        Me.Controls.Add(Me.DivButton)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.ResetShow)
+        Me.Controls.Add(Me.Button03)
+        Me.Controls.Add(Me.Button02)
+        Me.Controls.Add(Me.virgule)
+        Me.Controls.Add(Me.Button07)
+        Me.Controls.Add(Me.Button01)
+        Me.Controls.Add(Me.Button04)
+        Me.Controls.Add(Me.Result)
+        Me.Name = "Form1"
+        Me.Text = "Calculatrice"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Result As Button
+    Friend WithEvents Button04 As Button
+    Friend WithEvents Button02 As Button
+    Friend WithEvents Button03 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button05 As Button
+    Friend WithEvents Button06 As Button
+    Friend WithEvents Button01 As Button
+    Friend WithEvents Button07 As Button
+    Friend WithEvents Button08 As Button
+    Friend WithEvents Button09 As Button
+    Friend WithEvents DivButton As Button
+    Friend WithEvents SubButton As Button
+    Friend WithEvents PlusButton As Button
+    Friend WithEvents virgule As Button
+    Friend WithEvents ResetShow As Button
+    Friend WithEvents Reset As Button
+    Friend WithEvents RemoveOne As Button
+    Friend WithEvents showRes As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button0 As Button
+    Friend WithEvents MultiButton As Button
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents leftNum As Label
+    Friend WithEvents sign As Label
+    Friend WithEvents rightNum As Label
+End Class
---
+++ calculatrice/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+    ' Action au click sur un des boutons de 1 à 9 et des signes de calcules
+    Private Sub MultiButton_Click(sender As Object, e As EventArgs) Handles Button5.Click, virgule.Click, Button09.Click, Button08.Click, Button07.Click, Button06.Click, Button05.Click, Button04.Click, Button03.Click, Button02.Click, Button01.Click, Button0.Click
+
+        ' On caste le sender dans le type voulu
+        Dim btn As Button = CType(sender, Button)
+
+        ' On affiche le chiffre ou nombre sélectionné par l'utilisateur
+        If showRes.Text = 0 Then
+            showRes.Text = btn.Text
+        Else
+            showRes.Text += btn.Text
+        End If
+    End Sub
+
+    Private Sub Assign_sign_Click(sender As Object, e As EventArgs) Handles SubButton.Click, PlusButton.Click, MultiButton.Click, DivButton.Click
+
+        ' On caste le sender dans le type voulu
+        Dim btn As Button = CType(sender, Button)
+
+        ' Si leftNum n'est pas encore assigné
+        If leftNum.Text = "" Then
+            ' On ajoute le signe au premier nombre
+            sign.Text = btn.Text
+
+            ' On place le nombre à calculé dans le label
+            leftNum.Text = showRes.Text
+            showRes.Text = 0
+        End If
+
+    End Sub
+
+    Private Sub Result_Click(sender As Object, e As EventArgs) Handles Result.Click
+
+        rightNum.Text = showRes.Text
+
+        Dim result As Integer
+        Select Case sign.Text
+            Case "-"
+                result = Integer.Parse(leftNum.Text) - Integer.Parse(rightNum.Text)
+            Case "+"
+                result = Integer.Parse(leftNum.Text) + Integer.Parse(rightNum.Text)
+            Case "x"
+                result = Integer.Parse(leftNum.Text) * Integer.Parse(rightNum.Text)
+            Case "÷"
+                result = Integer.Parse(leftNum.Text) / Integer.Parse(rightNum.Text)
+        End Select
+        showRes.Text = result
+    End Sub
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        leftNum.Text = ""
+        rightNum.Text = ""
+        sign.Text = ""
+        showRes.Text = 0
+    End Sub
+
+    Private Sub RemoveOne_Click(sender As Object, e As EventArgs) Handles RemoveOne.Click
+
+        ' On supprime le dernier élément entré si le showRes n'est pas égal à 0
+        If showRes.Text <> "0" Then
+
+            ' Si il n'y a qu'un chiffre alors on affiche 0, sinon on le supprime
+            If showRes.Text.Length = 1 Then
+                showRes.Text = 0
+            Else
+                showRes.Text = showRes.Text.Substring(0, showRes.Text.Length - 1)
+            End If
+        End If
+    End Sub
+End Class
